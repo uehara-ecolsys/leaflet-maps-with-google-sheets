a283748953f8d00cb66c6422283ea047c2300ea0
uehara-ecolsys <80368342+uehara-ecolsys@users.noreply.github.com>
2022-06-10 11:41:52 +0900

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1no-DVq_v3-jsRVXB5-oPPd8eugLzpCBavR10WA3piFA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDYMD6wCDImAn1Ks_LVViP8rwCJPnv2sy0';
